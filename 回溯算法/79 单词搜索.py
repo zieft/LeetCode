a47b11e372635ed1d27cc066c3d7ad572379fdc9
Yulin Zhu <Y.Zhu@wzl.rwth-aachen.de>
2023-06-25 14:43:01 +0200

    79 回溯+dfs O(RC3^L) O(L)

--- 回溯算法/79 单词搜索.py
+++ 回溯算法/79 单词搜索.py
@@ -10,29 +10,41 @@
 
 class Solution:
     def exist(self, board: list[list[str]], word: str) -> bool:
+        """
+        回溯+深度优先搜索
+        时间复杂度为 O(RC3^L)
+        空间复杂度为 O(L)
+        其中 R 和 C 分别为网格的行数和列数，L 为单词的长度。
+        """
 
         def dfs(board, r, c, index):
             if not 0 <= r < len(board) or not 0 <= c < len(board[0]):
-                return
-            if ([r, c] in mask) and board[r, c] != index:
-                return
-            if board[r][c][0] == word[index]:
-                index += 1
-                return True
+                return  # 出界直接跳过
+            if ([r, c] in stack):
+                return  # 标记为已使用的跳过
+            stack.append([r, c])  # 标记当前节点为已使用
+            if board[r][c] == word[index]:
+                index += 1  # 如果和单词中对应的字母匹配，则接着找下一个字母
+            else:
+                stack.pop()  # 否则当前节点出栈，标记为未使用。
+                return False
 
-            mask.append([r, c])
-            dfs(board, r + 1, c, index)
-            dfs(board, r - 1, c, index)
-            dfs(board, r, c + 1, index)
-            dfs(board, r, c - 1, index)
-            mask.remove([r, c])
+            if index == len(word):  # 退出条件
+                return True  # 如果dfs的深度和word的长度相同，说明找到对应的单词
 
-        for r in range(len(board)):
+            if dfs(board, r + 1, c, index) or \
+                    dfs(board, r - 1, c, index) or \
+                    dfs(board, r, c + 1, index) or \
+                    dfs(board, r, c - 1, index):
+                return True  # 说明在任意方向上找到了对应的字母，并把True返回给上一层
+            stack.pop()  # 如果一个节点的四个方向上都没找到正确的字母，说明这条路走不通了，把当前这条路上的”已使用“回溯成”未使用“
+
+        for r in range(len(board)):  # for循环用来遍历不同的起点
             for c in range(len(board[0])):
-                index = 0
+                index = 0  # index用于记录dfs的深度
                 if board[r][c][0] == word[index]:
-                    mask = []
-                    if dfs(board, r, c, index) == True:
+                    stack = []  # 维护一个栈，用来记录已使用的节点
+                    if dfs(board, r, c, index) == True:  # dfs用来从不同的起点深入进去走路
                         return True
 
         return False
@@ -40,7 +52,15 @@ class Solution:
 
 if __name__ == '__main__':
     board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-    word = "ABCCED"
+    word = "ABCB"
     sol = Solution()
     res = sol.exist(board, word)
+    board2 = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+    word2 = "ABCCED"
+    res2 = sol.exist(board2, word2)
+    board3 = [["A", "B", "C", "E"], ["S", "F", "E", "S"], ["A", "D", "E", "E"]]
+    word3 = "ABCESEEEFS"
+    res3 = sol.exist(board3, word3)
     print(res)
+    print(res2)
+    print(res3)
