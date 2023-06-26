4aa36f2ba503dfde1f72f8591bc743d277a589ee
Yulin Zhu <Y.Zhu@wzl.rwth-aachen.de>
2023-06-26 13:36:26 +0200

    746 dp O(n) O(n)

---
+++ DynamicProgramming/746 使用最小花费爬楼梯.py
@@ -0,0 +1,39 @@
+"""
+给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。
+一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+
+你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+
+请你计算并返回达到楼梯顶部的最低花费。
+
+输入：cost = [10,15,20]
+输出：15
+解释：你将从下标为 1 的台阶开始。
+- 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+总花费为 15 。
+
+"""
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        """
+        dp[i]: 达到第i阶所需要的花费
+        O(n)
+        O(n)
+        """
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i - 1], dp[i - 2] + cost[i])
+
+        return min(dp[len(cost) - 1], dp[len(cost) - 2])
+
+
+if __name__ == '__main__':
+    cost = [10, 15, 20]
+    sol = Solution()
+    res = sol.minCostClimbingStairs(cost)
+    print(res)
